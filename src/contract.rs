d9c410d42d002496709ad0fec0f2f41c0dfbb50c
darkodi <darko.deuric@ethernal.tech>
2023-11-29 16:32:21 +0100

    entry point macro

--- src/contract.rs
+++ src/contract.rs
@@ -1,9 +1,14 @@
 use cosmwasm_std::{Response, StdResult};
-use sylvia::contract;
+use sylvia::{contract, entry_points};
 use sylvia::types::InstantiateCtx; //  context type
 
 pub struct CounterContract;
 
+// macro which dispatch received messages to the handler
+/*The #[entry_point] macro in Sylvia is like a translator, 
+enabling the complex Rust types used in smart contract functions to be understood and handled by the simpler Wasm runtime.  */
+// it goes before #[contract]!
+#[entry_points] 
 /*We mark the impl block with contract attribute macro. 
 It will parse every method inside the impl block marked with the [msg(...)] attribute and create proper messages 
 and utilities like multitest helpers for them. */
